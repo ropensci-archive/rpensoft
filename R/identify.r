15d65fd17e01f089186afdc473e31070fce7a285
Scott Chamberlain <myrmecocystus@gmail.com>
2012-09-03 09:29:19 -0700

    added identify fxn to identify the OAI-PMH pensoft service

---
+++ R/identify.r
@@ -0,0 +1,13 @@
+#' Learn about the Pensoft OAI-PMH service.
+#' 
+#' @import OAIHarvester
+#' @param url Base URL for the service.
+#' @return List of information describing the Pensoft OAI-PMH services.
+#' @export
+#' @examples \dontrun{
+#' identify()
+#' }
+identify <- function(url = "http://oai.pensoft.eu", transform = FALSE) 
+{
+    oaih_identify(url, transform = transform)
+}
